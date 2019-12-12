e63fe7a20ccd98928dfc7efccd041654019aa5d5
Ian Hoffman <ijh6@cornell.edu>
2019-12-12 02:27:08 -0800

    Refactor Day 12 Part 2

--- aoc_12/src/main.rs
+++ aoc_12/src/main.rs
@@ -18,8 +18,8 @@ struct Cli {
 
 #[derive(Clone, Copy, Debug)]
 struct Moon {
-    position: (i64, i64, i64),
-    velocity: (i64, i64, i64)
+    position: [i64; 3],
+    velocity: [i64; 3],
 }
 
 impl Moon {
@@ -31,12 +31,12 @@ impl Moon {
 
         if let Some(captures) = MOON_PARSE_REGEX.captures(s.as_str()) {
             Ok(Moon{
-                position: (
+                position: [
                     captures[1].parse::<i64>()?,
                     captures[2].parse::<i64>()?,
                     captures[3].parse::<i64>()?
-                ),
-                velocity: (0, 0, 0),
+                ],
+                velocity: [0, 0, 0],
             })
         } else {
             Err(From::from("Could not parse moon"))
@@ -44,32 +44,22 @@ impl Moon {
     }
 
     fn total_energy(&self) -> i64 {
-        (self.position.0.abs() + self.position.1.abs() + self.position.2.abs())
-        * (self.velocity.0.abs() + self.velocity.1.abs() + self.velocity.2.abs())
+        self.position.iter().map(|p| p.abs()).sum::<i64>()
+        * self.velocity.iter().map(|p| p.abs()).sum::<i64>()
     }
 
-    fn get_gravity_towards(&self, other: &Moon) -> (i64, i64, i64) {
-        (
-            (other.position.0 - self.position.0).signum(),
-            (other.position.1 - self.position.1).signum(),
-            (other.position.2 - self.position.2).signum()
-        )
+    fn get_gravity_towards(&self, other: &Moon) -> [i64; 3] {
+        let as_vec: Vec<i64> = other.position.iter().zip(self.position.iter())
+            .map(|(o, s)| (o - s).signum()).collect();
+        [as_vec[0], as_vec[1], as_vec[2]]
     }
 
-    fn apply_gravity(&mut self, gravity: (i64, i64, i64)) {
-        self.velocity = (
-            self.velocity.0 + gravity.0,
-            self.velocity.1 + gravity.1,
-            self.velocity.2 + gravity.2
-        );
+    fn apply_gravity(&mut self, gravity: [i64; 3], neg: bool) {
+        self.velocity.iter_mut().zip(gravity.iter()).for_each(|(v, g)| *v += if neg { -g } else { *g });
     }
 
     fn apply_velocity(&mut self) {
-        self.position = (
-            self.position.0 + self.velocity.0,
-            self.position.1 + self.velocity.1,
-            self.position.2 + self.velocity.2
-        );
+        self.position.iter_mut().zip(self.velocity.iter()).for_each(|(p, v)| *p += v);
     }
 }
 
@@ -77,19 +67,19 @@ fn simulate_moons(moons: &mut Vec<Moon>) {
     for i in 0..moons.len() {
         for j in i..moons.len() {
             let gravity = moons[i].get_gravity_towards(&moons[j]);
-            moons[i].apply_gravity(gravity);
-            moons[j].apply_gravity((-gravity.0, -gravity.1, -gravity.2));
+            moons[i].apply_gravity(gravity, false);
+            moons[j].apply_gravity(gravity, true);
         }
         moons[i].apply_velocity();
     }
 }
 
-fn moons_to_dimensional_hash_keys(moons: &Vec<Moon>) -> (String, String, String) {
-    let mut keys = ("".to_owned(), "".to_owned(), "".to_owned());
+fn moons_to_dimensional_hash_keys(moons: &Vec<Moon>) -> [String; 3] {
+    let mut keys = ["".to_owned(), "".to_owned(), "".to_owned()];
     for moon in moons {
-        keys.0.push_str(&format!("[{},{}]", moon.position.0, moon.velocity.0));
-        keys.1.push_str(&format!("[{},{}]", moon.position.1, moon.velocity.1));
-        keys.2.push_str(&format!("[{},{}]", moon.position.2, moon.velocity.2));
+        for i in 0..3 {
+            keys[i].push_str(&format!("[{},{}]", moon.position[i], moon.velocity[i]));
+        }
     }
     keys
 }
@@ -112,38 +102,27 @@ fn part1(moons: &mut Vec<Moon>, num_steps: usize) {
 }
 
 fn part2(moons: &mut Vec<Moon>) {
-    let mut seen_x_states: HashMap<String, usize> = HashMap::new();
-    let mut seen_y_states: HashMap<String, usize> = HashMap::new();
-    let mut seen_z_states: HashMap<String, usize> = HashMap::new();
-    let mut x_cycle: Option<usize> = None;
-    let mut y_cycle: Option<usize> = None;
-    let mut z_cycle: Option<usize> = None;
+    let mut seen_states: [HashMap<String, usize>; 3] = [HashMap::new(), HashMap::new(), HashMap::new()];
+    let mut cycle_lengths: [Option<usize>; 3] = [None, None, None];
     let mut num_steps = 0;
 
-    while x_cycle.is_none() || y_cycle.is_none() || z_cycle.is_none() {
-        let (x_key, y_key, z_key) = moons_to_dimensional_hash_keys(moons);
-
-        if let Some(v) = seen_x_states.get(&x_key) {
-            if x_cycle.is_none() { x_cycle = Some(num_steps - *v); }
-        }
-
-        if let Some(v) = seen_y_states.get(&y_key) {
-            if y_cycle.is_none() { y_cycle = Some(num_steps - *v); }
-        }
-
-        if let Some(v) = seen_z_states.get(&z_key) {
-            if z_cycle.is_none() { z_cycle = Some(num_steps - *v); }
+    while !cycle_lengths.iter().all(|l| l.is_some() ) {
+        let hash_keys = moons_to_dimensional_hash_keys(moons);
+        for i in 0..3 {
+            if cycle_lengths[i].is_none() {
+                if let Some(v) = seen_states[i].get(&hash_keys[i]) {
+                    cycle_lengths[i] = Some(num_steps - *v);
+                }
+                seen_states[i].insert(hash_keys[i].clone(), num_steps);
+            }
         }
 
         simulate_moons(moons);
-        if x_cycle.is_none() { seen_x_states.insert(x_key, num_steps); }
-        if y_cycle.is_none() { seen_y_states.insert(y_key, num_steps); }
-        if z_cycle.is_none() { seen_z_states.insert(z_key, num_steps); }
         num_steps += 1;
     }
 
-    let lcm_of_dimension_cycles = lcm(lcm(x_cycle.unwrap(), y_cycle.unwrap()), z_cycle.unwrap());
-    println!("Would return to previously seen state after {} simulation steps", lcm_of_dimension_cycles);
+    let lcm_across_dimensions = cycle_lengths.iter().fold(1, |acc, l| lcm(acc, l.unwrap()));
+    println!("Would return to previously seen state after {} simulation steps", lcm_across_dimensions);
 }
 
 fn main() -> Result<()> {
