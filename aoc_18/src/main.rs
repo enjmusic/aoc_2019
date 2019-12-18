68e308019da14484c911937dc5c103b4a0755c1a
Ian Hoffman <ijh6@cornell.edu>
2019-12-17 23:17:42 -0800

    TODO: Implement TopologicalOrderingIterator

--- aoc_18/src/main.rs
+++ aoc_18/src/main.rs
@@ -12,10 +12,41 @@ struct Cli {
     file: PathBuf,
 }
 
+struct TopologicalOrderingIterator {
+    dependencies: HashMap<char, HashSet<char>>,
+    in_degree: HashMap<char, usize>,
+    done: bool,
+}
+
+impl TopologicalOrderingIterator {
+    fn from_dependencies(deps: HashMap<char, HashSet<char>>) -> TopologicalOrderingIterator {
+        let mut in_degree = HashMap::new();
+        for (_, depended_on) in &deps {
+            for d in depended_on {
+                let entry = in_degree.entry(*d).or_insert(0);
+                *entry += 1;
+            }
+        }
+        TopologicalOrderingIterator{
+            dependencies: deps,
+            in_degree: in_degree,
+            done: false,
+        }
+    }
+}
+
+impl Iterator for TopologicalOrderingIterator {
+    type Item = Vec<char>;
+
+    fn next(&mut self) -> Option<Vec<char>> {
+        None
+    }
+}
+
 struct KeySolver {
     entrance_to_keys: HashMap<char, usize>,
     keys_to_keys: HashMap<char, HashMap<char, usize>>,
-    dependencies: HashMap<char, Vec<char>>,
+    dependencies: HashMap<char, HashSet<char>>,
 }
 
 impl KeySolver {
@@ -25,29 +56,35 @@ impl KeySolver {
         for (name, pos) in grid.keys.iter() {
             keys_to_keys.insert(*name, grid.bfs_to_keys(*pos, false).distances);
         }
+        let dependencies = deps_and_distances_from_entrance.dependencies.unwrap();
         KeySolver {
             entrance_to_keys: deps_and_distances_from_entrance.distances,
             keys_to_keys: keys_to_keys,
-            dependencies: deps_and_distances_from_entrance.dependencies.unwrap(),
+            dependencies: dependencies,
         }
     }
 
-    fn get_shortest_path(&self) -> usize {
-        // TODO - generate all topological sorts of the dependency
-        // graph and find the one that produces the minimum distance
-        0
+    fn get_shortest_path(&mut self) -> usize {
+        let mut shortest = std::usize::MAX;
+        for path in TopologicalOrderingIterator::from_dependencies(self.dependencies.clone()) {
+            let mut length = self.entrance_to_keys[&path[0]];
+            for i in 1..path.len() {
+                length += self.keys_to_keys[&path[i - 1]][&path[i]];
+            }
+            if length < shortest { shortest = length; }
+        }
+        shortest
     }
 }
 
 struct BFSResult {
     distances: HashMap<char, usize>,
-    dependencies: Option<HashMap<char, Vec<char>>>,
+    dependencies: Option<HashMap<char, HashSet<char>>>,
 }
 
 struct Grid {
     data: Vec<Vec<char>>,
     keys: HashMap<char, (usize, usize)>,
-    doors: HashMap<char, (usize, usize)>,
     entrance: (usize, usize),
 }
 
@@ -56,19 +93,16 @@ impl Grid {
         let mut data = vec![];
         let mut entrance = (0, 0);
         let mut keys: HashMap<char, (usize, usize)> = HashMap::new();
-        let mut doors: HashMap<char, (usize, usize)> = HashMap::new();
         for (row, line) in r.lines().enumerate() {
             data.push(line?.chars().collect::<Vec<char>>());
             for (col, c) in data.last().unwrap().iter().enumerate() {
                 if c.is_ascii_lowercase() { keys.insert(*c, (col, row)); }
-                if c.is_ascii_uppercase() { doors.insert(*c, (col, row)); }
                 if *c == '@' { entrance = (col, row); }
             }
         }
         Ok(Grid{
             data: data,
             keys: keys,
-            doors: doors,
             entrance: entrance,
         })
     }
@@ -76,17 +110,17 @@ impl Grid {
     fn bfs_to_keys(&self, start: (usize, usize), get_deps: bool) -> BFSResult {
         let mut visited: HashSet<(usize, usize)> = HashSet::new();
         // (distance from start, point to visit, doors seen so far i.e. dependencies)
-        let mut frontier: VecDeque<(usize, (usize, usize), Vec<char>)> = VecDeque::new();
-        frontier.push_front((0, start, vec![]));
+        let mut frontier: VecDeque<(usize, (usize, usize), HashSet<char>)> = VecDeque::new();
+        frontier.push_front((0, start, HashSet::new()));
 
         let mut distances: HashMap<char, usize> = HashMap::new();
-        let mut dependencies: HashMap<char, Vec<char>> = HashMap::new();
+        let mut dependencies: HashMap<char, HashSet<char>> = HashMap::new();
 
         while frontier.len() != 0 {
             let (mut dist, point, mut deps) = frontier.pop_back().unwrap();
             visited.insert(point);
             let curr_char = self.data[point.1][point.0];
-            if curr_char.is_ascii_uppercase() { deps.push(curr_char); }
+            if curr_char.is_ascii_uppercase() { deps.insert(curr_char); }
             if curr_char.is_ascii_lowercase() {
                 distances.insert(curr_char, dist);
                 dependencies.insert(curr_char, deps.clone());
@@ -118,7 +152,7 @@ fn main() -> Result<()> {
     let f = File::open(opt.file)?;
     let mut reader = BufReader::new(f);
     let grid = Grid::from_reader(&mut reader)?;
-    let solver = KeySolver::from_grid(&grid);
+    let mut solver = KeySolver::from_grid(&grid);
     println!("Minimum distance to get all keys: {}", solver.get_shortest_path());
 
     Ok(())
