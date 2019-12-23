f2f8af4783de51d7769c174b05926a1a32274bdb
Ian Hoffman <ijh6@cornell.edu>
2019-12-22 21:58:27 -0800

    Day 23 Part 1 & 2

---
+++ aoc_23/src/main.rs
@@ -0,0 +1,92 @@
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+use std::path::PathBuf;
+use std::collections::{HashMap, HashSet, VecDeque};
+use structopt::StructOpt;
+use intcode::program::{Event, IntcodeProgram};
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+#[derive(Debug, StructOpt)]
+struct Cli {
+    #[structopt(short = "f", parse(from_os_str))]
+    file: PathBuf,
+}
+
+fn run(input: &String) -> Result<()> {
+    let memory = IntcodeProgram::raw_to_memory(input)?;
+    let mut programs = (0..50).map(|addr| {
+        let mut program = IntcodeProgram::from_memory(memory.clone());
+        program.give_input(addr as i64);
+        program
+    }).collect::<Vec<IntcodeProgram>>();
+
+    let mut exited: HashSet<usize> = HashSet::new();
+    let mut buffered_out: Vec<Vec<i64>> = vec![vec![]; 50];
+    let mut packets: HashMap<usize, VecDeque<(i64, i64)>> = HashMap::new();
+    let mut nat_packet: Option<(i64, i64)> = None;
+    let mut last_nat_y: Option<i64> = None;
+    while exited.len() < 50 {
+        let mut idle = true;
+        for (idx, program) in programs.iter_mut().enumerate() {
+            if exited.contains(&idx) { continue }
+            match program.execute_until_event()? {
+                Event::Exited => { exited.insert(idx); println!("{} exited", idx); },
+                Event::InputRequired => {
+                    let entry = packets.entry(idx).or_insert(VecDeque::new());
+                    if let Some((x, y)) = entry.pop_back() {
+                        idle = false;
+                        program.give_input(x);
+                        program.give_input(y);
+                    } else {
+                        program.give_input(-1);
+                    }
+                },
+                Event::ProducedOutput => {
+                    idle = false;
+                    buffered_out[idx].push(program.get_output().unwrap());
+                    if buffered_out[idx].len() == 3 {
+                        let (addr, x, y) = (buffered_out[idx][0] as usize, buffered_out[idx][1], buffered_out[idx][2]);
+                        buffered_out[idx].clear();
+                        if addr < 50 {
+                            let entry = packets.entry(addr).or_insert(VecDeque::new());
+                            entry.push_front((x, y));
+                        } else if addr == 255 {
+                            nat_packet = Some((x, y));
+                        } else {
+                            return Err(From::from(format!("Invalid addr: {}", addr)));
+                        }
+                    }
+                },
+            }
+        }
+
+        if idle && nat_packet.is_some() {
+            let packet = nat_packet.unwrap();
+            nat_packet = None;
+
+            if last_nat_y.is_some() && packet.1 == last_nat_y.unwrap() {
+                println!("First repeated Y value sent by NAT: {}", packet.1);
+                break
+            } else if last_nat_y.is_none() {
+                println!("Y value of first packet sent to NAT: {}", packet.1);
+            }
+
+            let entry = packets.entry(0).or_insert(VecDeque::new());
+            entry.push_front(packet);
+            last_nat_y = Some(packet.1);
+        }
+    }
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let opt = Cli::from_args();
+
+    let f = File::open(opt.file)?;
+    let mut reader = BufReader::new(f);
+    let mut contents = String::new();
+    reader.read_to_string(&mut contents)?;
+
+    run(&contents)
+}
