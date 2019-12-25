e51fb39c4aa2b5be7c2b9abb2cea7e912874c2fa
Ian Hoffman <ijh6@cornell.edu>
2019-12-25 00:40:26 -0500

    Day 25 Done - that's a wrap!

--- aoc_25/src/main.rs
+++ aoc_25/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{prelude::*, BufReader};
+use std::io::{self, prelude::*, BufReader};
 use std::path::PathBuf;
 use structopt::StructOpt;
 use intcode::program::{Event, IntcodeProgram};
@@ -12,8 +12,34 @@ struct Cli {
     file: PathBuf,
 }
 
-fn part1(input: &String) -> Result<()> {
-    let program = IntcodeProgram::from_raw_input(input)?;
+fn get_input_line(program: &mut IntcodeProgram) -> Result<()> {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)?;
+    for c in input.trim().chars() {
+        program.give_input((c as u8) as i64);
+    }
+    program.give_input(10);
+    Ok(())
+}
+
+fn run_text_adventure(input: &String) -> Result<()> {
+    let mut program = IntcodeProgram::from_raw_input(input)?;
+    let mut output_buffer = vec![];
+    loop {
+        match program.execute_until_event()? {
+            Event::Exited => break,
+            Event::InputRequired => get_input_line(&mut program)?,
+            Event::ProducedOutput => {
+                match program.get_output().unwrap() {
+                    10 => {
+                        println!("{}", output_buffer.iter().collect::<String>());
+                        output_buffer.clear();
+                    },
+                    c => output_buffer.push((c as u8) as char)
+                }
+            },
+        }
+    }
     Ok(())
 }
 
@@ -25,6 +51,6 @@ fn main() -> Result<()> {
     let mut contents = String::new();
     reader.read_to_string(&mut contents)?;
     
-    part1(&contents)?;
-    Ok(())
+    // Weight required: food ration, space law space brochure, mutex, mouse, asterisk
+    run_text_adventure(&contents)
 }
